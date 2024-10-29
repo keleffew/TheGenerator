1b5eb818954ee897f71a33a6e8d8b970957950ad
Kevin Leffew <kevin@replit.com>
2024-10-29 16:10:33 +0000

    Assistant checkpoint

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-## print.("hello world")
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get-cat')
+def get_cat():
+    response = requests.get('https://api.thecatapi.com/v1/images/search')
+    data = response.json()
+    return data[0]['url']
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
